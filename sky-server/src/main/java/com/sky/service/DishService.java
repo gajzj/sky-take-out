d8b421315f33f955ba64fb6f86f0e6db00dffbab
GAJ <1304399919@qq.com>
2024-10-07 20:20:59 +0800

    feat: delete dish

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -5,6 +5,8 @@ import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.result.PageResult;
 
+import java.util.List;
+
 public interface DishService {
 
     /**
@@ -20,4 +22,15 @@ public interface DishService {
      * @return
      */
     PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    /**
+     * 菜品批量删除
+     * <p>
+     * 在 dish 表中删除菜品基本数据，同时把 dish_flavor 表中数据一起删除
+     * <p>
+     * 若删除的菜品数据关联某个套餐则删除失败
+     * 若要删除套餐关联的菜品数据，先接触关联，再删除
+     * @param ids
+     */
+    void deleteBatch(List<Long> ids);
 }
