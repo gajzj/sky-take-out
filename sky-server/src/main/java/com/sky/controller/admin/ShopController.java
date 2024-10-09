1ae46dc14afda1da1a46bc0128f854cd6dd234a1
GAJ <1304399919@qq.com>
2024-10-09 20:10:13 +0800

    feat: shop status

--- sky-server/src/main/java/com/sky/controller/admin/ShopController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -4,17 +4,15 @@ import com.sky.constant.StatusConstant;
 import com.sky.result.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.models.auth.In;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@RestController("adminShopController")
 @RequestMapping("/admin/shop")
-@Api("店铺操作接口")
+@Api(tags = "店铺操作接口")
 @Slf4j
 public class ShopController {
 
@@ -38,6 +36,15 @@ public class ShopController {
         return Result.success();
     }
 
-
+    /**
+     * 获取营业状态
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("获取营业状态")
+    public Result<Integer> getStatus() {
+        Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
+        return Result.success(status);
+    }
 
 }
