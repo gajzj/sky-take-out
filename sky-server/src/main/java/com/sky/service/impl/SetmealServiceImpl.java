0e1f960931de69ea177434250aaee10727c4b2ee
GAJ <1304399919@qq.com>
2024-10-09 10:22:18 +0800

    feat: 分页查询

--- sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -21,8 +21,12 @@ public class SetmealServiceImpl implements SetmealService {
     public PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO) {
         PageHelper.startPage(setmealPageQueryDTO.getPage(), setmealPageQueryDTO.getPageSize());
 
-        Page<SetmealVO> page = setmealMapper.pageQuery(setmealPageQueryDTO);
-
-        return new PageResult(page.getTotal(), page.getResult());
+        try {
+            Page<SetmealVO> page = setmealMapper.pageQuery(setmealPageQueryDTO);
+            return new PageResult(page.getTotal(), page.getResult());
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
     }
 }
